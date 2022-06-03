5122c04df7ef23e488bacbd65d83e7e04befc3a3
ashirvadtidake@gmail.com <ashirvadtidake@gmail.com>
2022-06-03 20:13:11 +0530

    Book data added in home page

---
+++ src/BookData.js
@@ -0,0 +1,78 @@
+export const SliderItems = [
+    {
+        id: 1,
+        img: "https://image.shutterstock.com/z/stock-vector-blue-cover-page-or-widescreen-layout-editable-vertical-template-design-for-marketing-presentation-1296690394.jpg",
+        title: "SUMMER SALE",
+        desc: " GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://images.indianexpress.com/2022/04/Amazon-Book-Sale.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/71whevleuhL._AC_UY218_.jpg",
+        title: "Book LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
+];
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/81kkQxSf4YL._AC_UY218_.jpg",
+        title: "Book Lover",
+    },
+    {
+        id: 2,
+        img: "https://m.media-amazon.com/images/I/816HBXHJsaL._AC_UY218_.jpg",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/811wuvCKC2S._AC_UY218_.jpg",
+    },
+    {
+        id: 4,
+        img: "https://m.media-amazon.com/images/I/81Ke+JJ1b5L._AC_UY218_.jpg",
+    },
+    {
+        id: 5,
+        img: "https://m.media-amazon.com/images/I/81WfDv1F96L._AC_UY218_.jpg",
+    },
+    {
+        id: 6,
+        img: "https://m.media-amazon.com/images/I/41qyHrI3ukL._AC_UY218_.jpg",
+    },
+    {
+        id: 7,
+        img: "https://m.media-amazon.com/images/I/51YxkXyzhqL._AC_UY218_.jpg",
+    },
+    {
+        id: 8,
+        img: "https://images-eu.ssl-images-amazon.com/images/I/81yswtkIMsL._AC_UL160_SR160,160_.jpg",
+    },
+]
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,42 +1,54 @@
 import React from 'react'
+import { SliderItems, categories, popularProducts } from '../../BookData.js'
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
 
 const Home = () => {
     return (
         <>
-            <div className="container my-5">
-                <h1 className="text-center">This is Homepage</h1>
 
+            <AwesomeSlider animation="cubeAnimation" style={{ height: 500 }}>
+                {SliderItems.map((item) => (
+                    <div data-src={item.img} style={{ background: item.bg }} key={item.id}>
+                        <h3>{item.title}</h3>
+                        <p>{item.desc}</p>
+                    </div>
+                ))}
+            </AwesomeSlider>
+            <div className="container my-5">
+                <h1 className="text-center mt-5">Categories</h1>
                 <div className="row">
-                    <div className="col-lg-4 col-md-12 col-sm-12">
-                        <div className="card mt-5">
-                            <div className="card-header">Card</div>
-                            <div className="card-body">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias nesciunt at, recusandae ex culpa tempora natus totam temporibus error reprehenderit ipsa! Voluptatem adipisci aut quasi quibusdam, officia iusto! Autem accusantium aspernatur fugiat pariatur sit quam, esse dolorum sequi temporibus expedita?
-                                </p>
+                    {categories.map((item) => (
+                        <div className="col-lg-4 col-md-12 col-sm-12" key={item.id}>
+                            <div className="card mt-5">
+                                <div className="card-body">
+                                    <img src={item.img} alt={item.img} style={{ width: '100%', height: '200px' }} />
+                                    <h3>{item.title}</h3>
+                                </div>
+                                {/* <div className="card-footer d-flex justify-content-between">
+                                    <button className="btn btn-info">Show Now</button>
+                                    <button className="btn btn-warning">Add to Cart</button>
+                                </div> */}
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12">
-                        <div className="card mt-5">
-                            <div className="card-header">Card</div>
-                            <div className="card-body">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias nesciunt at, recusandae ex culpa tempora natus totam temporibus error reprehenderit ipsa! Voluptatem adipisci aut quasi quibusdam, officia iusto! Autem accusantium aspernatur fugiat pariatur sit quam, esse dolorum sequi temporibus expedita?
-                                </p>
+                    ))}
+                </div>
+                <h1 className="text-center mt-5">Popular Products</h1>
+                <div className="row">
+                    {popularProducts.map((item) => (
+                        <div className="col-lg-4 col-md-12 col-sm-12" key={item.id}>
+                            <div className="card mt-5">
+                                <div className="card-body">
+                                    <img src={item.img} alt={item.img} style={{ width: '100%', height: '200px' }} />
+                                    <h3>{item.title}</h3>
+                                </div>
+                                <div className="card-footer d-flex justify-content">
+                                    <button className="btn btn-success">Add to Cart</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12">
-                        <div className="card mt-5">
-                            <div className="card-header">Card</div>
-                            <div className="card-body">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias nesciunt at, recusandae ex culpa tempora natus totam temporibus error reprehenderit ipsa! Voluptatem adipisci aut quasi quibusdam, officia iusto! Autem accusantium aspernatur fugiat pariatur sit quam, esse dolorum sequi temporibus expedita?
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </>
